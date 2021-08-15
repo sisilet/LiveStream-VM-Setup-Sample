9e20aaaab11c810b0166b24c74492a8bfcbe6dd0
sisilet <astroboykl@yahoo.com>
2021-08-15 20:32:27 +1000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,4 +2,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Install Software
-choco install microsoft-edge microsoft-teams obs-studio obs-ndi skype -y
+choco install microsoft-edge microsoft-teams obs-studio obs-ndi skype vb-cable -y
+
+Set-ItemProperty -Path "HKEY_CURRENT_USER\Software\Microsoft\ServerManager" -Name "CheckedUnattendLaunchSetting" -Value 1
